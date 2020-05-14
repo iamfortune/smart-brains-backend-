a312c72534f8ed221756844367241eaf133f54ff
iamfortune <ikechifortune@gmail.com>
2020-05-14 16:47:26 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sarah',
+      email: 'sarah@gmail.com',
+      password: 'cokies',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+
+app.get('/', (req, res)=> {
+  res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email && 
+    req.body.password === database.users[0].password) {
+      res.json('sucess');
+    } else {
+      res.status(400).json('error logging in')
+    }
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  })
+  res.json(database.users[database.users.length-1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      return res.json(user);
+    } 
+  })
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
+
