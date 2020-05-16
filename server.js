5860f8c41405170a4e63fdc95f67433bd81d6bfb
iamfortune <ikechifortune@gmail.com>
2020-05-16 10:43:37 +0100

    modified files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs'); 
 
 const app = express();
 
@@ -32,6 +33,14 @@ app.get('/', (req, res)=> {
 });
 
 app.post('/signin', (req, res) => {
+  // Load hash from your password DB.
+bcrypt.compare("bacon", hash, function (err, res) {
+  // res == true
+});
+bcrypt.compare("veggies", hash, function (err, res) {
+  // res = false
+});
+
   if (req.body.email === database.users[0].email && 
     req.body.password === database.users[0].password) {
       res.json('sucess');
@@ -55,12 +64,33 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
+  let found = false;
   database.users.forEach(user => {
     if (user.id === id) {
+      found = true;
       return res.json(user);
     } 
   })
+  if (!found) {
+    res.status(400).json('not found ');
+  }
 })
+
+app.put('/image', (req, res) => {
+   const { id } = req.body;
+   let found = false;
+   database.users.forEach((user) => {
+     if (user.id === id) {
+       found = true;
+       user.entries++
+       return res.json(user.entries);
+     }
+   })
+   if (!found) {
+     res.status(400).json("not found ");
+   }
+})
+
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
