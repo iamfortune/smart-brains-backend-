2e1e7a7e51d1adbd0a25ed132314e78504aa5156
iamfortune <ikechifortune@gmail.com>
2020-06-05 13:02:44 +0100

    added a register to make it cleaner and have a separation of concerns with the endpoints and server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 
 const db = knex({
   client: 'pg',
@@ -46,40 +47,11 @@ app.get('/', (req, res) => {
   res.send(database.users)
 }) 
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-  .where('email', '=', req.body.email)
-  .then(data => {
-    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-    if (isValid) {
-      return db.select('*').from('users')
-      .where('email', '=', req.body.email)
-      .then(user => {
-        res.json(user[0])
-      })
-      .catch(err => res.status(400).json('unable to get user'))
-    } else {
-      res.status(400).json("wrong username and password");
-    }
-  })
-  .catch(err => res.status(400).json('incorrect username and password!'))
-})
+app.post('/signin',(req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  let found = false;
- db.select('*').from('users').where({id})
- .then(user => {
-   if (user.length) {
-      res.json(user[0]);
-   } else {
-      res.status(400).json('Not found')
-   }
- })
- .catch(err => res.status(400).json('error getting user'))
-})  
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})  
 
 
 app.put('/image', (req, res) => {
