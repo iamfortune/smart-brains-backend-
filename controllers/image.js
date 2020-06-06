b848250efcbe0789c9ae0a88a646998e7fd64574
iamfortune <ikechifortune@gmail.com>
2020-06-06 10:44:42 +0100

    refactored more code to improve security

--- controllers/image.js
+++ controllers/image.js
@@ -1,18 +1,3 @@
-const Clarifai = require("clarifai");
-
-const app = new Clarifai.App({
-  apiKey: "d8bc081eb66f488b9e44206d01dee76c",
-});
-
-const handleApiCall = (req, res) => {
-  app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => res.status(400).json("unable to work with API"));
-};
-
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -26,6 +11,5 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-  handleImage,
-  handleApiCall,
-};
+    handleImage: handleImage
+}
--- controllers/register.js
+++ controllers/register.js
@@ -2,7 +2,7 @@ const handleRegister = (req, res, db, bcrypt) => {
   // added the objects to be registered here 
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json("incorrect form submission");
+    return res.status(400).json("please add details!");
   }
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
