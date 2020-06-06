e8b904186bd9c1dd3e164f67bec5781e402017a1
iamfortune <ikechifortune@gmail.com>
2020-06-06 10:05:46 +0100

    refactored the code for image

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "d8bc081eb66f488b9e44206d01dee76c",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -11,5 +26,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-    handleImage: handleImage
-}
+  handleImage,
+  handleApiCall,
+};
