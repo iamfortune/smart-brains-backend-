23995eb78fb136018922f6d54d6b6eee4ebf13c5
iamfortune <ikechifortune@gmail.com>
2020-06-09 00:00:53 +0100

    added a new endpoint for images

--- server.js
+++ server.js
@@ -57,6 +57,10 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })  
 // Get a user profile and id 
 
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 // Call to clarifai
 
