c07f5460473105ed6cfdcdaec35f6a62a8d5ad2c
iamfortune <ikechifortune@gmail.com>
2020-06-09 12:33:07 +0100

    added the heroku postgresql

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-amorphous-67345',
     user : 'kaycodev', 
     password : 'kaycodev20',
     database : 'smart-brain'
